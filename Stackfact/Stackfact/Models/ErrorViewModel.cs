8da563a2f2f26e52e2972a0929ef33267930b126
jus4u <justinjoseph500@gmail.com>
2021-10-19 00:03:56 +0530

    this is a test

---
+++ Stackfact/Stackfact/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stackfact.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
